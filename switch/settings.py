69d17918a6971490041085c59bb1349cacf4391f
maheshwari <archit.maheshwari@payufin.com>
2022-09-07 23:54:14 +0530

    signing in with email instead of username

--- switch/settings.py
+++ switch/settings.py
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'account',
 ]
 
 MIDDLEWARE = [
@@ -104,6 +105,8 @@ AUTH_PASSWORD_VALIDATORS = [
         'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
     },
 ]
+
+AUTH_USER_MODEL = 'account.Account'
 
 
 # Internationalization
