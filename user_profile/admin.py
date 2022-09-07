1cb9b2e4872316acc93aab1279f3fe42b13046bd
maheshwari <archit.maheshwari@payufin.com>
2022-09-08 01:24:50 +0530

    Education and ProfExp Model

--- user_profile/admin.py
+++ user_profile/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
 
-from .models import Profile
+from user_profile.models import Profile, ProfessionalExperience, Education
 # Register your models here.
 admin.site.register(Profile)
+admin.site.register(ProfessionalExperience)
+admin.site.register(Education)
--- user_profile/models.py
+++ user_profile/models.py
@@ -2,6 +2,12 @@ from django.db import models
 
 from account.models import Account
 
+DegreeChoices = (
+    ('NA','NA'),
+    ('BTECH', 'BTECH'),
+    ('MTECH', 'MTECH'),
+)
+
 # Create your models here.
 class Profile(models.Model):
     created_on = models.DateTimeField(auto_now_add=True)
@@ -20,3 +26,30 @@ class Profile(models.Model):
         return self.user.email 
 
     
+class Education(models.Model):
+    created_on = models.DateTimeField(auto_now_add=True)
+    updated_on = models.DateTimeField(auto_now=True)
+    profile = models.ForeignKey(Profile, on_delete=models.CASCADE)
+    college = models.CharField(max_length=50, null=True)
+    branch = models.CharField(max_length=30, null=True)
+    graduation_year = models.IntegerField()
+    cgpa = models.FloatField()
+    #TODO: Think of a better default
+    degree = models.CharField(max_length=20, choices= DegreeChoices, default='NA')
+
+    def __str__(self):
+        return str(self.profile.user.email) + " " + str(self.college)
+
+class ProfessionalExperience(models.Model):
+    created_on = models.DateTimeField(auto_now_add=True)
+    updated_on = models.DateTimeField(auto_now=True)
+    profile = models.ForeignKey(Profile, on_delete=models.CASCADE)
+    company_name = models.CharField(max_length=50)
+    years_of_experience = models.FloatField()
+    # will introduce this after definition of Role
+    # role = models.ForeignKey(Role, on_delete=models.CASCADE) 
+
+    def __str__(self):
+        return self.profile.user.email + " " + self.company_name
+
+
